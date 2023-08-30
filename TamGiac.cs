787ecc076af79378eb08515033db398a2febb870
Phong <nnphong130303@gmail.com>
2023-08-30 16:13:26 +0700

    TamGiac

--- TamGiac.cs
+++ TamGiac.cs
@@ -8,5 +8,35 @@ namespace buoi3
 {
     class TamGiac
     {
+        private int canhMA;
+        private int canhMB;
+        private int canhMC;
+        private double ChuVi;
+        private double DienTich;
+
+        public TamGiac()
+        {
+            canhMA = 0;
+            canhMB = 0;
+            canhMC = 0;
+        }
+        public TamGiac(int canhMA, int canhMB, int canhMC)
+        {
+            this.canhMA = canhMA;
+            this.canhMB = canhMB;
+            this.canhMC = canhMC;
+        }
+        public double ChuVi()
+        {
+            return canhMC + canhMA + canhMB;
+        }
+        public double nuaChuVi()
+        {
+            return (canhMA + canhMB + canhMC) / 2;
+        }
+        public double DienTich()
+        {
+            return Math.Sqrt(nuaChuVi * (nuaChuVi - canhMA) * (nuaChuVi - canhMB) * (nuaChuVi - canhMC));
+        }
     }
 }
